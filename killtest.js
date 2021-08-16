6d660605e3c36fb0ff0704086f63ecc8f1b3adca
marutimuthu <marutimuthu@gmail.com>
2021-08-16 11:03:56 +0530

    update

--- killtest.js
+++ killtest.js
@@ -1,5 +1,6 @@
 const { exec } = require('child_process');
 const killchrome = "killall chromium-browser"
+const restart1Command = "/opt/dashboard-o-26/killall.sh"
 
 exec(killchrome, (err, stdout, stderr) => {
     console.log(`[ RESTARTING ${stdout} ]`);
